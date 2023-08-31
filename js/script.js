4566b3fda9e074a7c1e3769acd1a37224b79bbb2
Yasamin(Yasmin) <83816430+yasamin-zandi@users.noreply.github.com>
2023-08-31 17:58:06 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var type= new Typed('.typing',{
-    strings:['am a web developer','am an AI Researcher','used to be a Python Developer',''],
+    strings:['am a Data Scientist','am an AI Researcher','used to be a Python Developer',''],
     typeSpeed:100,
     Backspeed:60,
     loop:true
